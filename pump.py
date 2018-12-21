be184d627a651e24c93c4b73039e040892cb0610
Erik van Zwol <pa2ees42@gmail.com>
2018-12-21 14:31:29 -0700

    Updated with new statuses, and methods to read/write statuses

--- pump.py
+++ pump.py
@@ -29,10 +29,19 @@ SETTINGS_ADDRESS = 0
 SETTINGS_TANK_PUMP_TURN_ON_LEVEL = 1
 SETTINGS_TANK_PUMP_TURN_OFF_LEVEL = 2
 
+STATUS_OP_WRITE = 2
 STATUS_OP_READ = 3
+
 
 STATUS_TEMP = 0
 STATUS_TANK_LEVEL = 1
+STATUS_FILLING = 2
+STATUS_PUMPING = 3
+STATUS_FILLING_VALVE_CURRENT = 4
+STATUS_PUMP_CURRENT = 5
+STATUS_NUM_OF_STATUSES = 6
+
+STATUS_ERROR = 255
 
 class Payload(object):
     """
@@ -259,12 +268,57 @@ class Pump(object):
     #### Helper functions ####
 
     def read_curr_temp(self):
-        resp_pkt = self.get_status(STATUS_TEMP)
+        resp_pkt = self.read_status(STATUS_TEMP)
         return self._conv_temp_to_f(resp_pkt.payload.value)
 
     def read_tank_level(self):
-        resp_pkt = self.get_status(STATUS_TANK_LEVEL)
+        resp_pkt = self.read_status(STATUS_TANK_LEVEL)
         return self._conv_tank_level_to_inches(resp_pkt.payload.value)
+
+    def read_filling(self):
+        resp_pkt = self.read_status(STATUS_FILLING)
+        return resp_pkt.payload.value
+
+    def read_pumping(self):
+        resp_pkt = self.read_status(STATUS_PUMPING)
+        return resp_pkt.payload.value
+
+    def read_filling_valve_current(self):
+        resp_pkt = self.read_status(STATUS_FILLING_VALVE_CURRENT)
+        return resp_pkt.payload.value
+
+    def read_pump_current(self):
+        resp_pkt = self.read_status(STATUS_PUMP_CURRENT)
+        return resp_pkt.payload.value
+
+    def filling_tanks_on(self):
+        resp_pkt = self.write_status(STATUS_FILLING, 1)
+        if resp_pkt.payload.value == 1:
+            print("Tanks are now filling")
+        else:
+            print("Failed to start filling tanks, response was: {}".format(resp_pkt.payload.value))
+
+    def filling_tanks_off(self):
+        resp_pkt = self.write_status(STATUS_FILLING, 0)
+        if resp_pkt.payload.value == 0:
+            print("Tanks are now NOT filling")
+        else:
+            print("Failed to stop filling, response was: {}".format(resp_pkt.payload.value))
+            
+    def pumping_on(self):
+        resp_pkt = self.write_status(STATUS_PUMPING, 1)
+        if resp_pkt.payload.value == 1:
+            print("Pump is now on")
+        else:
+            print("Failed to turn on pump, response was: {}".format(resp_pkt.payload.value))
+
+    def pumping_off(self):
+        resp_pkt = self.write_status(STATUS_PUMPING, 0)
+        if resp_pkt.payload.value == 0:
+            print("Pump is now off")
+        else:
+            print("Failed to turn off pump, response was: {}".format(resp_pkt.payload.value))
+
 
     def read_setting_address(self):
         resp_pkt = self.read_setting(SETTINGS_ADDRESS)
@@ -308,6 +362,7 @@ class Pump(object):
         else:
             print("Setting 'turn off level' programming failed!")
 
+    
     def load_all_settings_from_eeprom(self):
         self.load_settings_from_eeprom()
 
@@ -331,6 +386,7 @@ class Pump(object):
     def read_setting(self, setting):
         pkt = Packet(dest_addr=self.addr, src_addr=self.src_addr, payload_type=PAYLOAD_TYPE_SETTINGS, payload_data=bytes([SETTINGS_OP_READ, setting, 3, 3]))
         resp_pkt = self._send_and_receive_packet(pkt)
+        log.debug("Resp_pkt: {}".format(resp_pkt))
         return resp_pkt
 
     def write_setting(self, setting, val):
@@ -362,9 +418,16 @@ class Pump(object):
             resp_pkt = self._send_and_receive_packet(pkt)
             return resp_pkt
 
-    def get_status(self, status):
+    def read_status(self, status):
         pkt = Packet(dest_addr=self.addr, src_addr=self.src_addr, payload_type=PAYLOAD_TYPE_STATUS, payload_data=bytes([STATUS_OP_READ, status, 2, 3]))
         resp_pkt = self._send_and_receive_packet(pkt)
+        return resp_pkt
+
+    def write_status(self, status, value):
+        pkt = Packet(dest_addr=self.addr, src_addr=self.src_addr, payload_type=PAYLOAD_TYPE_STATUS, payload_data=bytes([STATUS_OP_WRITE, status, value & 0xFF, (value >> 8) & 0xFF]))
+        resp_pkt = self._send_and_receive_packet(pkt)
+        if resp_pkt.payload.command == STATUS_ERROR:
+            print("Node had an error handling packet")
         return resp_pkt
 
     def _send_and_receive_packet(self, pkt):
